bf28d010e1e4a6092b5460d7d3e1b14ada1e00bb
alex-romac <39037629+alex-romac@users.noreply.github.com>
2022-09-29 15:25:34 -0500

    Create assesment porject

---
+++ src/main/java/com/bp/cbe/CbePracticalAssesmentJava2022Q3Application.java
@@ -0,0 +1,13 @@
+package com.bp.cbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbePracticalAssesmentJava2022Q3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbePracticalAssesmentJava2022Q3Application.class, args);
+	}
+
+}
